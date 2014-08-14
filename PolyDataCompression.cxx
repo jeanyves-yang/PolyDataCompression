92cc5511cb7297e6070f5651fadcf51414f8c059
jeanyves-yang <jean-yves.yang@cpe.fr>
2014-08-14 11:29:25 -0400

    STYLE: code cleaned, xml tag more specific (string-enum)

---
+++ PolyDataCompression.cxx
@@ -0,0 +1,127 @@
+#include "PolyDataCompressionCLP.h"
+#include "string.h"
+#include <iostream>
+#include <sstream>
+#include <cstring>
+#include <vtkSmartPointer.h>
+#include <vtkPolyData.h>
+#include <vtkPolyDataReader.h>
+#include <vtkPolyDataWriter.h>
+#include <vtkXMLPolyDataReader.h>
+#include <vtkXMLPolyDataWriter.h>
+#include <vtkZLibDataCompressor.h>
+
+std::string ExtensionOfFile( std::string fileName ) //use ITK
+{
+    std::string extension ;
+    extension = fileName.substr( fileName.find_last_of(".")+1,fileName.size()-fileName.find_last_of( "." ) + 1 ) ;
+    return extension ;
+}
+
+std::string ChangeEndOfFileName ( std::string fileName, std::string change )
+{
+    std::string extension = ExtensionOfFile( fileName ) ;
+    fileName.replace( fileName.end()-extension.length()-1 , fileName.end() , change ) ;
+    fileName = fileName + ".vtp" ;
+    return fileName;
+}
+
+vtkSmartPointer< vtkPolyData > ReadFile( std::string fileName , std::string extension )
+{
+    if( extension == "vtk" )
+    {
+        vtkSmartPointer< vtkPolyDataReader > reader = vtkSmartPointer< vtkPolyDataReader >::New() ;
+        reader->SetFileName( fileName.c_str() ) ;
+        reader->Update() ;
+        return reader->GetOutput() ;
+    }
+    else if( extension == "vtp" )
+    {
+        vtkSmartPointer< vtkXMLPolyDataReader > reader = vtkSmartPointer< vtkXMLPolyDataReader >::New() ;
+        reader->SetFileName( fileName.c_str() ) ;
+        reader->Update() ;
+        return reader->GetOutput() ;
+    }
+    std::cout << "input file is not a vtk or a vtp file." << std::endl ;
+
+    exit( 1 ) ;
+}
+
+void WriteFile( std::string encoding , std::string outputFileName , int compressionLevel , vtkSmartPointer< vtkPolyData > readerPolyData )
+{
+    vtkSmartPointer< vtkXMLPolyDataWriter > writer = vtkSmartPointer< vtkXMLPolyDataWriter >::New() ;
+    writer->SetFileName( outputFileName.c_str() ) ;
+    writer->SetInputData( readerPolyData ) ;
+    vtkZLibDataCompressor *compressor = dynamic_cast<vtkZLibDataCompressor*> (writer->GetCompressor()) ;
+    if( compressor )
+    {
+        compressor->SetCompressionLevel( compressionLevel ) ;
+    }
+    if( !strcmp( encoding.c_str() , "binary" ) )
+    {
+        writer->SetDataModeToBinary();
+    }
+    else if(!strcmp( encoding.c_str() , "appended" ) )
+    {
+        writer->SetDataModeToAppended();
+    }
+    else
+    {
+        writer->SetDataModeToAscii();
+    }
+    writer->Update() ;
+}
+
+int main( int argc, char *argv[] )
+{
+    std::vector <std::string> fileNameList;
+    std::string flagFileName = "-f" ;
+    int count = 1 ;
+    while( strcmp( argv[ count ] , flagFileName.c_str() )  )
+    {
+        if( count >= argc-1 )
+        {
+            std::cout << "Missing -f flag" <<std::endl ;
+            std::cout << "USAGE:" << std::endl << "     ./VTKCompression  [-e] [<std::string>] "
+                      << std::endl << "                       [-c <integer>] "
+                      << std::endl << "                       [-f <std::vector<std::string>>] "
+                      << std::endl << "                       <std::string>] [--] [--version] [-h]" << std::endl ;
+            return EXIT_FAILURE ;
+        }
+        count ++ ;
+    }
+    count ++ ;
+    while( count < argc )
+    {
+        if( argv[ count ][ 0 ] == '-' )
+        {
+            std::cout<<"Please move any flags before the -f flag. -f flag should be the last flag. "<<std::endl;
+            return EXIT_FAILURE ;
+        }
+        else
+        {
+            fileNameList.push_back( argv[ count ] ) ;
+        }
+        count ++ ;
+    }
+    argc=argc-fileNameList.size() - 1 ;
+    PARSE_ARGS ;
+    std::string append = "-compressed" ;
+    for( size_t i=0 ; i <  fileNameList.size() ; i++ )
+    {
+        if(  fileNameList[i] == append.c_str() )
+        {
+            std::cout << fileNameList[ i ] << " already compressed." << std::endl ;
+            continue ;
+        }
+        else
+        {
+            vtkSmartPointer< vtkPolyData > readerPolyData = vtkSmartPointer< vtkPolyData >::New() ;
+            std::string outputFileName = ChangeEndOfFileName( fileNameList[ i ] , append ) ;
+            readerPolyData = ReadFile( fileNameList[ i ] , ExtensionOfFile( fileNameList[ i ] ) ) ;
+            WriteFile( encoding , outputFileName , compressionLevel , readerPolyData ) ;
+        }
+    }
+    return EXIT_SUCCESS ;
+}
+
