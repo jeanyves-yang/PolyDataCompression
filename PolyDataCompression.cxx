fb88abc4bab8493b974a77875876a7315cec1d09
jeanyves-yang <jean-yves.yang@cpe.fr>
2014-08-14 16:39:25 -0400

    STYLE: while loops merged into a single for loop for clarity

--- PolyDataCompression.cxx
+++ PolyDataCompression.cxx
@@ -12,37 +12,37 @@
 #include <vtkZLibDataCompressor.h>
 #include <itksys/SystemTools.hxx>
 
-std::string ChangeEndOfFileName ( std::string& fileName, std::string& change )
+std::string ChangeEndOfFileName ( std::string fileName, std::string change )
 {
     return  itksys::SystemTools::GetFilenamePath( fileName )
             + "/" + itksys::SystemTools::GetFilenameWithoutLastExtension( fileName ) + change + ".vtp" ;
 }
 
-vtkSmartPointer< vtkPolyData > ReadFile( std::string fileName , std::string extension )
+vtkSmartPointer< vtkPolyData > ReadFile( const char* fileName , std::string extension )
 {
     if( extension == ".vtk" )
     {
         vtkSmartPointer< vtkPolyDataReader > reader = vtkSmartPointer< vtkPolyDataReader >::New() ;
-        reader->SetFileName( fileName.c_str() ) ;
+        reader->SetFileName( fileName ) ;
         reader->Update() ;
         return reader->GetOutput() ;
     }
     else if( extension == ".vtp" )
     {
         vtkSmartPointer< vtkXMLPolyDataReader > reader = vtkSmartPointer< vtkXMLPolyDataReader >::New() ;
-        reader->SetFileName( fileName.c_str() ) ;
+        reader->SetFileName( fileName ) ;
         reader->Update() ;
         return reader->GetOutput() ;
     }
     std::cout << "input file is not a vtk or a vtp file." << std::endl ;
-
-    exit( 1 ) ;
+    return NULL ;
 }
 
-void WriteFile( std::string encoding , std::string outputFileName , int compressionLevel , vtkSmartPointer< vtkPolyData > readerPolyData )
+int WriteFile( std::string encoding , const char* outputFileName ,
+                int compressionLevel , vtkSmartPointer< vtkPolyData > readerPolyData )
 {
     vtkSmartPointer< vtkXMLPolyDataWriter > writer = vtkSmartPointer< vtkXMLPolyDataWriter >::New() ;
-    writer->SetFileName( outputFileName.c_str() ) ;
+    writer->SetFileName( outputFileName ) ;
     writer->SetInputData( readerPolyData ) ;
     vtkZLibDataCompressor *compressor = dynamic_cast< vtkZLibDataCompressor* > ( writer->GetCompressor() ) ;
     if( compressor )
@@ -57,62 +57,54 @@ void WriteFile( std::string encoding , std::string outputFileName , int compress
     {
         writer->SetDataModeToAppended() ;
     }
-    else if( encoding =="ascii" )
+    else if( encoding == "ascii" )
     {
         writer->SetDataModeToAscii() ;
     }
     else // should not arrive here. tested in main before already and program should exit at that time.
     {
-        std::cout << "encoding parameter should be set as binary, appended or ascii." << std::endl ;
-        exit( 1 ) ;
+        return 1 ;
     }
     writer->Update() ;
+    return 0 ;
 }
 
 int main( int argc, char *argv[] )
 {
-    std::vector< std::string > fileNameList;
-    std::string flagFileName = "-f" ;
-    int count = 1 ;
-    while( strcmp( argv[ count ] , flagFileName.c_str() )  )
+    std::vector< std::string > fileNameList ;
+    bool flagFound = false ;
+    for( int i = 0 ; i < argc ; i++ )
     {
-        if( count >= argc-1 )
+        if( flagFound )
         {
-            std::cout << "Missing -f flag" << std::endl ;
-            std::cout << "USAGE:" << std::endl << "     ./VTKCompression  [-e] [<std::string>] "
-                      << std::endl << "                       [-c <integer>] "
-                      << std::endl << "                       [-f <std::vector<std::string>>] "
-                      << std::endl << "                       <std::string>] [--] [--version] [-h]" << std::endl ;
-            return EXIT_FAILURE ;
+            if( argv[ i ][ 0 ] == '-' )
+            {
+                std::cout << "Please write all your flags before the -f flag. -f flag should be the last flag. "<< std::endl ;
+                return EXIT_FAILURE ;
+            }
+            else
+            {
+              fileNameList.push_back( argv[ i ] ) ;
+            }
         }
-        count ++ ;
+        if( !strcmp( argv[ i ] , "-f" ) || !strcmp( argv[ i ] , "--fileNameList" ) )
+        {
+            flagFound = true ;
+        }
     }
-    count ++ ;
-    while( count < argc )
+    if( !flagFound )
     {
-        if( argv[ count ][ 0 ] == '-' )
-        {
-            std::cout << "Please write all your flags before the -f flag. -f flag should be the last flag. "<< std::endl ;
-            return EXIT_FAILURE ;
-        }
-        else
-        {
-            fileNameList.push_back( argv[ count ] ) ;
-        }
-        count ++ ;
+       std::cout << "Missing fileNameList flag, please give at least one file." << std::endl ;
+       return EXIT_FAILURE ;
     }
-    argc=argc-fileNameList.size() - 1 ;
+    argc = argc - ( fileNameList.empty() ? 0 : fileNameList.size() - 1 ) ;
     PARSE_ARGS ;
-    if( encoding != "binary" && encoding != "appended" && encoding != "ascii" )
-    {
-        std::cout << "encoding parameter should be set as binary, appended or ascii." << std::endl ;
-        return EXIT_FAILURE ;
-    }
+
     std::string append = "-compressed" ;
     for( size_t i = 0 ; i < fileNameList.size() ; i++ )
     {
-        std::string strippedFileName = itksys::SystemTools::GetFilenameWithoutLastExtension( fileNameList[ i ] ) ;
-        if( strippedFileName.find_last_of( append ) != strippedFileName.length()-1 ) //looks if "-compressed" is already at the end of the filename> if yes, the file will be skipped
+        std::string testFileName = itksys::SystemTools::GetFilenameName( ( fileNameList[ i ] ) ) ;
+        if( testFileName.find( append + "vtk" ) != std::string::npos || testFileName.find( append + "vtk" ) != std::string::npos ) //looks if "-compressed" is already at the end of the filename> if yes, the file will be skipped
         {
             std::cout << fileNameList[ i ] << " already compressed." << std::endl ;
             continue ;
@@ -121,8 +113,15 @@ int main( int argc, char *argv[] )
         {
             vtkSmartPointer< vtkPolyData > readerPolyData = vtkSmartPointer< vtkPolyData >::New() ;
             std::string outputFileName = ChangeEndOfFileName( fileNameList[ i ] , append ) ;
-            readerPolyData = ReadFile( fileNameList[ i ] , itksys::SystemTools::GetFilenameLastExtension( fileNameList[ i ] ) ) ;
-            WriteFile( encoding , outputFileName , compressionLevel , readerPolyData ) ;
+            readerPolyData = ReadFile( fileNameList[ i ].c_str() , itksys::SystemTools::GetFilenameLastExtension( fileNameList[ i ] ) ) ;
+            if( !readerPolyData.GetPointer() )
+            {
+                return EXIT_FAILURE ;
+            }
+            if( WriteFile( encoding , outputFileName.c_str() , compressionLevel , readerPolyData )  == 1 )
+            {
+                return EXIT_FAILURE ;
+            }
         }
     }
     return EXIT_SUCCESS ;
